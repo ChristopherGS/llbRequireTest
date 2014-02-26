60f6120a1bcafe99fb52ec139d8f65832e736708
ChristopherGS <ChristopherGS@users.noreply.github.com>
2014-02-26 11:48:52 +0800

    Update main.js

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -2,19 +2,7 @@ require.config({
 
 	//baseUrl: 'scripts',
 	paths: {
-		jquery: '../bower_components/jquery/jquery',
-		bootstrap: 'bower_components/bootstrap/dist/js/bootstrap'
-	},
-	
-	shim: {
-		underscore: {
-		  exports: '_'
-		},
-		bootstrap: {
-		  deps: ['jquery'],
-		  exports: 'jQuery'
-		}
-		
+		jquery: '../bower_components/jquery/jquery'
 	}
 
 });
